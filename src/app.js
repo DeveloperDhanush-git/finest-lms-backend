d8997fd6c74311349c2d6bb3f76401636f170798
Dhanush <dhanushlingam6363@gmail.com>
2026-06-09 12:57:45 +0530

    Initial backend setup

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const healthRoute = require('./routes/health.route');
+
+app.use('/api/health', healthRoute);
+
+app.get('/', (req, res) => {
+    console.log('Received a request to the root endpoint');
+    res.send('Welcome to the Finest LMS API!');
+});
+
+module.exports = app;
